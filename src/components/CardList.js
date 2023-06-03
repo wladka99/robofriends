1de5a8ce18fd5312df3880a811d6786d92c9a2da
wladka99 <132443931+wladka99@users.noreply.github.com>
2023-06-03 11:12:39 +0200

    commit

---
+++ src/components/CardList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card.js"
+
+const CardList = ({robots}) => {
+  if(true) {
+    throw new Error("nooo");
+  }
+    return (
+    <div>
+        { 
+        robots.map((user, i) => {
+        return (
+        <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}
+        />
+        );
+        })
+    }
+    </div>    
+    );
+}
+export default CardList;
